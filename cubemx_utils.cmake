8bea72e2304950c253995669896e85d663d09a66
Jan Macheta <jan.macheta@embe.tech>
2023-11-29 15:26:43 +0100

    unify cmake version, and fix typo

--- cubemx_utils.cmake
+++ cubemx_utils.cmake
@@ -1,3 +1,5 @@
+cmake_minimum_required(VERSION 3.21)
+
 set(CUBEMX_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR} CACHE INTERNAL "cubemx-cmake source directory")
 
 include(${CUBEMX_CMAKE_DIR}/find_cubemx.cmake)
